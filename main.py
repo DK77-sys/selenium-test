9bf853e4932cf78bd1bc2f5401301fd84142812e
Ahmad <58675446+DK77-sys@users.noreply.github.com>
2022-01-22 10:47:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from selenium import webdriver
 
 op = webdriver.ChromeOptions()
-op.binary.location = os.environ.get("GOOGLE_CHROME_BIN")
+op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 op.add_argument("--headless")
 op.add_argument("--no-sandbox")
 op.add_argument("--disable-dev-sh-usage")
