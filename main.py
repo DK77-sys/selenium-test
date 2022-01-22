b4d8e79ca4464bcc0166c5fed7f8a04f48a6abee
Ahmad <58675446+DK77-sys@users.noreply.github.com>
2022-01-22 11:26:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from selenium import webdriver
 from flask import  *
+import flask
 
 op = webdriver.ChromeOptions()
 op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
@@ -8,18 +9,30 @@ op.add_argument("--headless")
 op.add_argument("--no-sandbox")
 op.add_argument("--disable-dev-sh-usage")
 
+# driver = webdriver.Chrome(executable_path="../webdriver/chromedriver.exe",chrome_options=op)
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=op)
 
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 app.secret_key = b'BB,^z\x90\x88?\xcf\xbb'
 
-@app.route('/', methods=['GET'])
+
+@app.route('/',methods=['GET'])
 def home():
-    url = request.args['url']
-    d = driver.get(url)
-    result = d.page_source
-    return result
+    return """
+<p>Ghost Scrapper Rendered JS</p>
+<footer>Coded By Ghost Saint</footer>
+"""
+
+@app.route('/scrap', methods=['GET'])
+def scrap():
+    if request.args.get('url'):
+        url = request.args['url']
+        driver.get(url)
+        # print(driver.page_source)
+        return driver.page_source
+    else:
+        return "Please Insert Url Param!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT','5000')),debug=True)
